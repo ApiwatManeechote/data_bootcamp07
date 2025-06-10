b11151bd9a6ad0f2d4285f97cd3ff326ee8a1004
Apiwat Maneechote <140755113+ApiwatManeechote@users.noreply.github.com>
2025-06-11 04:41:33 +0700

    Update data_cleansing.sql
    Finishing the data cleansing.

--- data_cleansing.sql
+++ data_cleansing.sql
@@ -19,7 +19,7 @@ WHERE composer IS NULL;
 
 -- 3. Trim whitespaces out 
 -- for multiples whitespaces in the part like in first name.
--- Firstname: "Frank " or " Frank" where customer might added 1 additional whitespace.
+-- Firstname: "Johnny " or " Johnny" where customer might added 1 additional whitespace.
 UPDATE customers
 SET firstname = TRIM(firstname);
 
@@ -29,9 +29,9 @@ UPDATE customers
 SET country = LOWER(country);
 
 -- 5. Fix misspelling and typos
-UPDATE your_table
-SET category = 'Books'
-WHERE category IN ('books', 'bokks', 'book');
+UPDATE customers
+SET country = 'USA'
+WHERE country IN ('Usa', 'U.S.A.', 'United States of America', 'usa');
 
 -- 6. Change data types
 -- We have to duplicates data from the old tables to a new table like this. 
@@ -41,7 +41,7 @@ CREATE TABLE invoice_items_new (
     InvoiceLineId INT,
     InvoiceId INT,
     TrackId INT,
-    UnitPrice REAL,
+    UnitPrice REAL, -- the old column's type was NUMERIC. So, let's change it to REAL.
     Quantity INT,
 );
 
@@ -53,3 +53,73 @@ DROP TABLE invoice_items_old;
 
 -- When we are finished changing data types, we can simply change the old name, like nothing happens.
 ALTER TABLE invoice_items_new RENAME TO invoice_items
+
+
+-- THAI LANGUAGE
+-- There'll be some input in Thai that requires specific data cleansing process such as address, name titles, etc.
+-- Let's say there's a student info table and we would like to do the data cleansing in the address column.
+-- And our goal is to maintain the address format like 'บ้านเลขที่ 999 ถนนพระราม 4 แขวงคลองเตย เขตคลองเตย กทม. 10110'.
+
+Create Table student_info (
+	student_id INT PRIMARY KEY,
+	firstname CHAR(150),
+	lastname CHAR(150),
+	address CHAR(200)
+);
+
+INSERT INTO student_info VALUES
+	(1, 'เสก', 'โลโซ', 'บ้านเลขที่ 999 ถนนพระราม 4 แขวงคลองเตย คลองเตย กรุงเทพฯ 10110'),
+	(2, 'ตูน', 'บอดี้', 'บ้านเลขที่999 ถนนพระราม4 คลองเตย เขตคลองเตย กรุงเทพ 10110'),
+	(3, 'ปั๊บ', 'โปเตโต้', 'เลขที่ 999 พระราม4 คลองเตย คลองเตย กทม. 10110'),
+	(4, 'โต', 'ซิลลี่', '999 พระราม 4 คลองเตย กทม 10110')
+;
+
+/* I'll split into 4 parts to make it look more clean about where we cleanse. 
+   Part 1: The House No. */
+UPDATE student_info
+SET address = 'บ้านเลขที่ ' || address
+WHERE address GLOB '[0-9]*';
+
+UPDATE student_info
+SET address = 'บ้าน' || address
+WHERE address LIKE 'เลขที่%';
+
+UPDATE student_info
+SET address = 'บ้านเลขที่ ' || SUBSTR(address, 11)
+WHERE address LIKE 'บ้านเลขที่%' AND SUBSTR(address, 11, 1) BETWEEN '0' AND '9';
+
+-- Part 2: The Road
+UPDATE student_info
+  SET address = REPLACE(address, 'ถนนพระราม4', 'ถนนพระราม 4');
+UPDATE student_info 
+  SET address = REPLACE(address, 'พระราม4', 'ถนนพระราม 4') WHERE address NOT LIKE '%ถนน%';
+UPDATE student_info 
+  SET address = REPLACE(address, 'พระราม 4', 'ถนนพระราม 4') WHERE address NOT LIKE '%ถนน%';
+
+-- Part 3: The District & The Subdistrict
+UPDATE student_info
+	SET address = REPLACE(address, 'คลองเตย เขตคลองเตย', 'แขวงคลองเตย เขตคลองเตย')
+WHERE address NOT LIKE '%แขวง%';
+
+UPDATE student_info
+	SET address = REPLACE(address, 'แขวงคลองเตย คลองเตย', 'แขวงคลองเตย เขตคลองเตย')
+WHERE address NOT LIKE '%เขต%';
+
+UPDATE student_info
+	SET address = REPLACE(address, 'คลองเตย คลองเตย', 'แขวงคลองเตย เขตคลองเตย')
+WHERE address NOT LIKE '%แขวง%' AND address NOT LIKE '%เขต%';
+
+UPDATE student_info
+	SET address = REPLACE(address, 'คลองเตย', 'แขวงคลองเตย เขตคลองเตย')
+WHERE address NOT LIKE '%แขวงคลองเตย%' AND address NOT LIKE '%เขตคลองเตย%';
+
+-- Part 4: The BKK Province
+UPDATE student_info 
+  SET address = REPLACE(address, 'กรุงเทพฯ', 'กทม.');
+UPDATE student_info 
+  SET address = REPLACE(address, 'กรุงเทพ', 'กทม.');
+UPDATE student_info 
+  SET address = REPLACE(address, 'กทม', 'กทม.') WHERE address NOT LIKE '%กทม.%';
+
+-- Now, you can see the updated address from our data cleansing process.
+SELECT * From student_info
